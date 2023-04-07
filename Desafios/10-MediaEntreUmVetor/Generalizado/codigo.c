aca5c22a3136b769f5aeddb58e3f29d6c8c47519
Miguel Antonio Beber <anim4.mus1c8@gmail.com>
2023-04-07 16:08:07 -0300

    Create codigo.c

---
+++ Desafios/10-MediaEntreUmVetor/Generalizado/codigo.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// Função principal do programa
+void main(){
+
+    setlocale(LC_ALL, "");
+
+    int tamanho;
+    printf("Qual será o tamanho do vetor? ");
+    scanf("%d", &tamanho);
+    float vetor[tamanho], soma;
+
+    for(int cont = 0; cont < tamanho; cont++){
+        printf("Digite um valor: "); scanf("%f", &vetor[cont]);
+        soma = soma + vetor[cont];
+    }
+    for(int cont = 0; cont < tamanho; cont++){
+        printf("\nVetor[%d] = %.1f", cont, vetor[cont]);
+    }
+    // Pula linhas
+    printf("\n");
+    printf("\nA média é = %.2f", soma / tamanho);
+}
