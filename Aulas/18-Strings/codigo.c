ee4f52189e08596e9866e8d2377e8682f06a7e36
Miguel Antonio Beber <anim4.mus1c8@gmail.com>
2023-04-08 21:52:28 -0300

    Create codigo.c

---
+++ Aulas/18-Strings/codigo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+// Função principal do programa
+void main(){
+
+
+    //Permirte usar acentos
+    setlocale(LC_ALL, "");
+
+    // Variaveis
+    char [255]
+
+    //Pausa o programa
+    system("pause");
+}
